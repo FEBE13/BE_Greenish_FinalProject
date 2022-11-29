0bb812898f9074636611c31605daf2f074032f83
DaffaRadh <daffasven@gmail.com>
2022-11-29 10:40:51 +0700

    cors add

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express()
 const bodyParser = require("body-parser");
 const allRouter = require('./Routers')
 const db = require('./config/db')
+var cors = require('cors')
 
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(allRouter)
